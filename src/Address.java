402718de97522044a4fc82a5dfba9bfc7475592a
szsunyuan <forrest.sun527@gmail.com>
2014-11-06 16:22:27 -0500

    Update Address.java
    added the set and get method.

--- src/Address.java
+++ src/Address.java
@@ -48,5 +48,29 @@ public class Address
      }
      
      private boolean isValidPostalCode( String postalCode ) { return false; }
+	public int getUnit() {
+		return unit;
+	}
+	public void setUnit(int unit) {
+		this.unit = unit;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getPostalCode() {
+		return postalCode;
+	}
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
  
 } 
