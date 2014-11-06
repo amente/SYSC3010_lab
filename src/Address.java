029a8b80e50eef54cd63bd8f4589c4b2c2f96f4a
unknown <amentebullo@cb5109-46.labs.sce.carleton.ca>
2014-11-06 15:28:27 -0500

    Initial commit

---
+++ src/Address.java
@@ -0,0 +1,52 @@
+
+public class Address
+{
+	public static int NAME_TEXT_LENGTH = 50;
+    private int unit;
+    private String street;
+    private String city;
+    private String postalCode;
+
+    public Address (String street, String city, String postalCode) throws Exception
+    { 
+    	if (street.length() ==0 || street.length()>NAME_TEXT_LENGTH){
+    		throw new Exception();
+    	}else{
+    		this.street = street;
+    	}
+    	
+    	if(!postalCode.matches("^[ABCEGHJKLMNPRSTVXY]{1}\\d{1}[A-Z]{1} *\\d{1}[A-Z]{1}\\d{1}$")){
+    		throw new Exception();
+    	}else{
+    		this.postalCode = postalCode;
+    	}
+    	
+    	if (city.length() ==0 || city.length()>NAME_TEXT_LENGTH){
+    		throw new Exception();
+    	}else{
+    		this.city = city;
+    	}
+    	
+    	
+    };
+    public Address (int unit, String street, String city, String postalCode) throws Exception
+    { 
+    	this(street,city,postalCode);
+    	this.unit = unit;    	
+    };
+
+     public String toString() { 
+    	 return Integer.toString(unit)+" "+street+"\n"+city+" "+postalCode;
+     }
+     public boolean equals(Object o) { 
+    	 if(o instanceof Address){
+    		 Address a = (Address)o;
+    		 return a.unit == this.unit && a.city.equalsIgnoreCase(this.city) && 
+    				 a.postalCode.equalsIgnoreCase(this.postalCode)&&a.street.equalsIgnoreCase(this.street);
+    	 }
+    	 return false;
+     }
+     
+     private boolean isValidPostalCode( String postalCode ) { return false; }
+ 
+} 
